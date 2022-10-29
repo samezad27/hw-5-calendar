c3c6f6153e66febabb1b91e1dfe02a919f07bdef
samuel ezadmanesh <samezad27@gmail.com>
2022-10-29 13:16:32 -0700

    starting to pseudo code the JS

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+//FUNCTION 
+$(document).ready(function () {
+    //display day and time on page
+
+        //save clicks
+
+        //get time and description values
+
+        //save to local storage
+
+        //notification of save to local storage
+
+        //remove show class after timeout 
+
+    //create function to update hours
+
+        // 
+  });
+
